c8a0b5008ede108b6765456b3125aa3e649da536
Julian <Powellj387@lynchburg.edu>
2023-02-16 08:49:14 -0500

    hm

--- src/cards/Hand.java
+++ src/cards/Hand.java
@@ -1,4 +1,32 @@
 package cards;
+import java.lang.Object;
+import java.util.Comparator;
 
 public class Hand {
+    public Hand(Comparator<Card> aCardComparator){
+    }
+
+    public void addCard(Card aCard){
+
+    }
+
+    public void removeCard(Card aCard){
+
+    }
+
+    public boolean contains(Card aCard){
+
+    }
+
+    public Card get(int index) throws ULIndexOutOfBoundsException{
+
+    }
+
+    public int size(){
+
+    }
+
+    public String toString(){
+
+    }
 }
--- src/cards/HeartsComparator.java
+++ src/cards/HeartsComparator.java
@@ -1,4 +1,6 @@
 package cards;
+import java.lang.Object;
+import java.util.Comparator;
 
 public class HeartsComparator {
 }
